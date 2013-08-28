388879354900295dbd27fdb3b55282a8a75e47d6
Jared Getzoff <jargetz@gmail.com>
2013-08-28 11:32:18 +0800

    initial commit

---
+++ parse/cloud/app.js
@@ -0,0 +1,45 @@
+
+// These two lines are required to initialize Express in Cloud Code.
+var express = require('express');
+var app = express();
+
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'ejs');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+
+// This is an example of hooking up a request handler with a specific request
+// path and HTTP verb using the Express routing API.
+app.get('/hello', function(req, res) {
+  res.render('hello', { message: 'Congrats, you just set up your app!' });
+});
+app.get('/map', function(req, res) {
+  //Get last 72 hours of locations
+  var pingRate = 30; //30 minutes
+  var pingsPer24Hours = 216 / (pingRate/60);
+  var Location = Parse.Object.extend("Location");
+  var query = new Parse.Query(Location);
+  query.limit(pingsPer24Hours);
+  query.descending('createdAt');
+  query.find({
+    success: function(results) {
+      res.render('map', { locations: results });
+    }
+  });
+
+});
+
+// // Example reading from the request query string of an HTTP get request.
+// app.get('/test', function(req, res) {
+//   // GET http://example.parseapp.com/test?message=hello
+//   res.send(req.query.message);
+// });
+
+// // Example reading from the request body of an HTTP post request.
+// app.post('/test', function(req, res) {
+//   // POST http://example.parseapp.com/test (with request body "message=hello")
+//   res.send(req.body.message);
+// });
+
+// Attach the Express app to Cloud Code.
+app.listen();
